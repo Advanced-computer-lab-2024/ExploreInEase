f5284c31ca6f27bd73fd92776280e62f3e45cc75
shahdmohamedfawzy <153244870+shahdmohamedfawzy@users.noreply.github.com>
2024-09-29 15:06:05 +0300

    all new files

---
+++ frontend/exploreinease/src/Shared/Components/AdminCard/adminsCard.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import Box from '@mui/material/Box';
+import CardActions from '@mui/material/CardActions';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import CardMedia from '@mui/material/CardMedia';
+import CardActionArea from '@mui/material/CardActionArea';
+import SettingsApplicationsSharpIcon from '@mui/icons-material/SettingsApplicationsSharp';
+
+interface AdminsCardProps {
+    title: string;
+    description: string;
+    imageUrl: string;
+  }
+  const AdminsCard: React.FC<AdminsCardProps> = ({ title, description, imageUrl, icon }) => {
+    return (
+        <Box>
+        <Card sx={{ display: 'flex' ,alignItems: 'center',justifyContent: 'center', margin: 2 }}>
+        <CardMedia
+          component="img"
+          sx={{ width: 140 }} 
+          image={imageUrl}
+          alt="green iguana"
+        />
+        <CardContent sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
+          <Typography gutterBottom variant="h5" component="div">
+          {title}
+          </Typography>
+          <Typography variant="body2" sx={{ color: 'text.secondary' }}>
+          {description}
+          </Typography>
+        </CardContent>
+        <CardActions sx={{ display: 'flex', alignItems: 'center', justifyContent: 'flex-end' }}>
+        <IconButton color="primary" aria-label="options"    
+           sx={{ 
+         size: '5rem', // Change this value to increase or decrease the size
+        padding: '16px', // Adjust padding for more clickable area
+      }}  >
+        <SettingsApplicationsSharpIcon sx={{ fontSize:45 }} />
+      </IconButton>
+        </CardActions>
+      </Card>
+      </Box>
+    )
+};
+export default AdminsCard;
