16fe7772c9d189d3e68746a8bf545ae4eb897b2d
shahdmohamedfawzy <153244870+shahdmohamedfawzy@users.noreply.github.com>
2024-10-03 01:14:25 +0300

    all profiles and the UsersPage for Admin

--- frontend/exploreinease/src/Shared/Components/AdminCard/adminsCard.js
+++ frontend/exploreinease/src/Shared/Components/AdminCard/adminsCard.js
@@ -1,10 +1,5 @@
 import * as React from 'react';
 import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Box from '@mui/material/Box';
-import CardActions from '@mui/material/CardActions';
-import IconButton from '@mui/material/IconButton';
-import Typography from '@mui/material/Typography';
 import CardMedia from '@mui/material/CardMedia';
 import CardActionArea from '@mui/material/CardActionArea';
 import SettingsApplicationsSharpIcon from '@mui/icons-material/SettingsApplicationsSharp';
