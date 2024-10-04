3ce54124f25b39e4093005eb32481ac4c2ebd757
mahmoudd2 <mahmouddahmedd220@gmail.com>
2024-10-04 19:22:40 +0300

    getTouristUpcommingEvents

--- backend/src/components/users/userController.js
+++ backend/src/components/users/userController.js
@@ -70,6 +70,31 @@ const fetchAllUsersAndTourists = async (req, res) => {
     }
 };
 
+//get all tourist upcomming activity
+const getTouristUpcommingEvents = async (req, res) => {
+  const { username } = req.params;
+
+  // Check if username is not a string
+  if (typeof username !== "string") {
+    return res.status(400).json({ error: "Username must be a string" });
+  }
+  if (!username) {
+    return res.status(400).json({ message: "username is required" });
+  }
+  try {
+    const events = await userService.getTouristUpcommingEvents(username);
+    return res.status(200).json(events);
+  } catch (error) {
+    return res.status(500).json({ error: "An error occurred", details: error.message });
+  }
+};
 
 
-module.exports = { deleteUserByIdAndType ,addGovernorOrAdmin,fetchAllUsersAndTourists};
+
+
+module.exports = {
+  deleteUserByIdAndType,
+  addGovernorOrAdmin,
+  fetchAllUsersAndTourists,
+  getTouristUpcommingEvents
+};
