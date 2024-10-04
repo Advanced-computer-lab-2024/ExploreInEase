492bfaa379ca05cd28c43b2708c672fc8ac7b1ac
mahmoudd2 <mahmouddahmedd220@gmail.com>
2024-10-04 19:36:11 +0300

    userService Update

--- backend/src/components/users/userService.js
+++ backend/src/components/users/userService.js
@@ -45,6 +45,67 @@ const fetchAllUsersAndTourists = async () => {
 };
 
 
+const getTouristUpcommingEvents = async (username) => {
+  const tourist = await userRepository.getTouristEventDetails(username);
 
-module.exports = { deleteUserByIdAndType, addGovernorOrAdmin, fetchAllUsersAndTourists,getUserById, deleteUserByIdAndType };
+  if (!tourist) {
+    throw new Error("Tourist not found");
+  }
+
+  const currentDate = new Date();
+
+  // Fetch activities, itineraries, and historical places
+  const activities = await eventRepository.getActivitiesByIds(
+    tourist.activityId
+  );
+  const itineraries = await eventRepository.getItinerariesByIds(
+    tourist.itinerary
+  );
+  const historicalPlaces = await eventRepository.getHistoricalPlacesByIds(
+    tourist.historicalplaceId
+  );
+
+  // Filter upcoming Events using date
+  const upcomingActivities = activities.filter(
+    (activity) => new Date(activity.date) >= currentDate
+  );
+  const upcomingItineraries = itineraries.filter(
+    (itinerary) => new Date(itinerary.date) >= currentDate
+  );
+  const upcomingHistoricalPlaces = historicalPlaces.filter(
+    (place) => new Date(place.date) >= currentDate
+  );
+
+  // Return the upcoming events as objects in JSON format
+  return {
+    upcomingActivities: upcomingActivities.map((activity) => ({
+      id: activity._id,
+      name: activity.name,
+      date: activity.date,
+      description: activity.description,
+    })),
+    upcomingItineraries: upcomingItineraries.map((itinerary) => ({
+      id: itinerary._id,
+      title: itinerary.title,
+      date: itinerary.date,
+      description: itinerary.description,
+    })),
+    upcomingHistoricalPlaces: upcomingHistoricalPlaces.map((place) => ({
+      id: place._id,
+      name: place.name,
+      date: place.date,
+      description: place.description,
+    })),
+  };
+};
+
+
+module.exports = {
+  deleteUserByIdAndType,
+  addGovernorOrAdmin,
+  fetchAllUsersAndTourists,
+  getUserById,
+  deleteUserByIdAndType,
+  getTouristUpcommingEvents
+};
 
