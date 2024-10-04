b82ee5db062095593da8d4ac384e4d5a9f6449c5
mahmoudd2 <mahmouddahmedd220@gmail.com>
2024-10-05 01:06:22 +0300

    main.js update

--- backend/main.js
+++ backend/main.js
@@ -1,12 +1,12 @@
-require('dotenv').config();
-
 const express = require('express');
 const mongoose = require('mongoose');
 const userRoutes = require('./src/components/users/userRoutes');
 const eventRoutes = require('./src/components/events/eventRoutes');
-const checkoutRoutes = require('./src/components/checkouts/checkoutsRoutes') 
-const setupSwaggerDocs = require('../backend/src/swagger/swagger');
- 
+const checkoutRoutes = require('./src/components/checkouts/checkoutsRoutes');
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+const fs = require('fs'); // Import filesystem module
+require('dotenv').config({ path: "src/.env" });
 
 // Express app
 const ACLapp = express();
@@ -14,17 +14,15 @@ const ACLapp = express();
 // Middleware to parse JSON
 ACLapp.use(express.json());
 
+// Logging middleware (optional, just for debugging)
 ACLapp.use((req, res, next) => {
-    console.log(req.path, req.method);
     next();
 });
 
 // Routes
 ACLapp.use(userRoutes);
 ACLapp.use(eventRoutes);
-ACLapp.use(checkoutRoutes);
-// Setup Swagger
-setupSwaggerDocs(ACLapp); 
+ACLapp.use(checkoutRoutes)
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
@@ -32,9 +30,37 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
         // Start listening for requests
         ACLapp.listen(process.env.PORT, () => {
             console.log(`Connected to DB & listening on port ${process.env.PORT}`);
-            console.log(`Swagger docs available at http://localhost:${process.env.PORT}/api-docs`); // Log Swagger docs URL
         });
     })
     .catch((error) => {
         console.log('Error connecting to MongoDB:', error);
+    });
+
+    const swaggerOptions = {
+        swaggerDefinition: {
+            openapi: "3.0.0", // OpenAPI version
+            info: {
+                title: "User Registration API",
+                version: "1.0.0",
+                description: "API for ACL Project",
+            },
+            servers: [
+                {
+                    url: "http://localhost:3030", // Update to your server URL
+                },
+            ],
+        },
+        apis: ["./src/swagger/swagger.js"], // Path to the API docs
+    };
+    const swaggerDocs = swaggerJsDoc(swaggerOptions);
+
+    // Serve swagger documentation
+    ACLapp.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+    fs.writeFileSync('./swagger.json', JSON.stringify(swaggerDocs, null, 2), (err) => {
+        if (err) {
+            console.error('Error writing swagger.json:', err);
+        } else {
+            console.log('swagger.json has been saved!');
+        }
     });
