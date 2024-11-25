81a70d9b2436a937757fdedf31b495fce79289ce
Saifahmedd <saifahmeddsalah@gmail.com>
2024-11-25 20:39:42 +0200

    models added for Ali

---
+++ backend/src/models/models/notification.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const { Schema } = mongoose;
+
+// Define the Notification Schema
+const notificationSchema = new Schema({
+    body: {
+        type: String,
+    },
+    user: {
+        user_id: {
+            type: mongoose.Schema.Types.ObjectId,
+        },
+        user_type: {
+            type: String,
+        }
+    }
+}, {
+    timestamps: true,               // Automatically add createdAt and updatedAt fields
+    versionKey: false                // Disable the "__v" version key
+});
+
+// Create the Notification Model
+const Notification = mongoose.models.Notification || mongoose.model('Notification', notificationSchema);
+
+module.exports = Notification;
---
+++ backend/src/models/models/order.js
@@ -0,0 +1,60 @@
+const mongoose = require("mongoose");
+const Tourist = require("../models/tourist");
+const Products = require("../models/product");
+const Schema = mongoose.Schema;
+
+const orderSchema = new Schema(
+  {
+    touristId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Tourist", // Reference to the Tourist model
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["delivered", "pending"],
+      default: "pending",
+      required: true,
+    },
+    productIds: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Products", // Reference to the Product model
+        required: true,
+      },
+    ],
+    productsIdsQuantity: [{
+      id: { type: mongoose.Schema.Types.ObjectId, ref: 'Products' }, // Reference to Itinerary schema
+      quantity: { type: Number} // Ensure price is not negative
+    }],
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+      min: 0                     // Price cannot be negative
+    },
+    dateDelivered: {
+      type: Date,
+      
+    },
+    addressToBeDelivered: {
+      
+        street: { type: String },
+        city: { type: String},
+        country: { type: String},
+        zipCode: { type: String, match: [/^\d{5}$/, 'Invalid zip code'] }
+    
+    },
+    paymentType: {
+      type: String,
+      enum: ["wallet", "COD", "card"],
+      required: true,
+    },
+  },
+  {
+    timestamps: true, // bey7ot adds createdAt and updatedAt fields beta3 el order
+  }
+);
+
+const Order = mongoose.models.Order ||mongoose.model("Order", orderSchema);
+
+module.exports = Order;
