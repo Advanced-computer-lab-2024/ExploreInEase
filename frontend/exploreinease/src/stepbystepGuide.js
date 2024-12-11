b7317d3971f4f093fe00cfa6209b5d8e2d993ab5
shahdmohamedfawzy <153244870+shahdmohamedfawzy@users.noreply.github.com>
2024-12-11 01:14:35 +0000

    wait for vod

--- frontend/exploreinease/src/stepbystepGuide.js
+++ frontend/exploreinease/src/stepbystepGuide.js
@@ -1,8 +1,13 @@
-import TouristNavbar from "./Tourist/TouristNavbar";
+import React from 'react';
+import HomePage from './Guest/GuestNavbar';
 const stepbystepGuide=()=>{ 
 return(
     <div>
-
+        <HomePage/>
+      <video width="600" controls>
+        <source src="path-to-your-video-file.mp4" type="video/mp4" />
+                Your browser does not support the video tag.
+      </video>
     </div>
 );
 };
